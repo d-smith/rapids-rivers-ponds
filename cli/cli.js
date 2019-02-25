759e04886229e5bff7816b570be4e66034cf7225
d-smith <doug.smith.mail@gmail.com>
2019-02-25 14:54:54 -0800

    Added set stream command

--- cli/cli.js
+++ cli/cli.js
@@ -1,15 +1,31 @@
 const vorpal = require('vorpal')();
 
-
+var streamName;
 
 const dispatchSend = async (args, callback) => {
+    if(streamName == undefined) {
+        vorpal.log('use the set stream command before dispatching');
+        callback();
+        return;
+    }
+
     console.log(args);
+    callback();
+}
+
+const setStream = async(args, callback) => {
+    console.log(`stream is ${args.stream}`);
+    streamName = args.stream;
     callback();
 }
 
 vorpal
     .command('send <event> [data]', 'Publish event with optional data')
     .action(dispatchSend);
+
+    vorpal
+    .command('set stream <stream>', 'Set name of stream to write events to')
+    .action(setStream);
 
 vorpal
     .delimiter('cmd >')
