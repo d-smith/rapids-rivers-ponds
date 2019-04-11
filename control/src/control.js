fe21bf06ead8bd6830bb7381b9f9bcd68b40748c
d-smith <doug.smith.mail@gmail.com>
2019-04-11 14:09:26 -0700

    stubbed handler for subscribe command

--- control/src/control.js
+++ control/src/control.js
@@ -6,6 +6,26 @@ let parseInput = (recordData) => {
     return JSON.parse(text);
 }
 
+let dispatchCommand = (cmd) => {
+    switch(cmd.command) {
+        case 'subscribe':
+            processSubscribe(cmd);
+            break;
+        default:
+            console.log(`Command not supported: ${JSON.stringify(cmd)}`);
+            break;
+    }
+}
+
+let processSubscribe = (cmd) => {
+    console.log(JSON.stringify(cmd));
+    let river = cmd.commandArgs.river;
+    let topic = cmd.commandArgs.topic;
+
+    console.log(`subscribe ${river} to ${topic}`);
+    console.log('check river for stage ' + process.env.STAGE);
+} 
+
 const handler = async(event, context) => {
     console.log(JSON.stringify(event));
     console.log(JSON.stringify(context));
@@ -13,7 +33,7 @@ const handler = async(event, context) => {
     for(rec of event['Records']) {
         try {
             parsed = parseInput(rec.kinesis.data);
-            console.log(parsed);
+            dispatchCommand(parsed);
         } catch(e) {
             console.log(e); //This throws away the record - might want to write it to a DLQ
         }
