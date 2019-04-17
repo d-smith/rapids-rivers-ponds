2c4eac429d22c46335a4504560b2b577214a4223
d-smith <doug.smith.mail@gmail.com>
2019-04-17 09:31:57 -0700

    Writing events to subscribed rivers

--- streams/src/stream.js
+++ streams/src/stream.js
@@ -1,12 +1,19 @@
 const AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB();
+var sqs = new AWS.SQS();
 
-let parseInput = (recordData) => {
+const decodeInput = (recordData) => {
     let buff = new Buffer(recordData, 'base64'); 
-    let text = buff.toString('utf-8');
-
-    return JSON.parse(text);
+    return buff.toString('utf-8');
 }
+
+
+
+const formRiverQName = (river) => {
+    return `${river}${process.env.STAGE}`;
+}
+
+
 
 const getSubscribersForEvent = async (eventType) => {
     let params = {
@@ -25,15 +32,38 @@ const getSubscribersForEvent = async (eventType) => {
     return response['Items'].map(x => x['Subscriber']['S']);
 }
 
+const write2river = async (subPayloadTuple) => {
+    let riverName = formRiverQName(subPayloadTuple.s);
+    console.log(`write payload to ${riverName}`);
+
+    return sqs.getQueueUrl({
+        QueueName: riverName
+    }).promise().then(result => {
+        return sqs.sendMessage({
+            QueueUrl: result['QueueUrl'],
+            MessageBody: JSON.stringify(subPayloadTuple.decoded)
+        }).promise();
+    }, reason => {
+        throw new Error(reason);
+    });
+}
+
 const handler = async (event) => {
     console.log(JSON.stringify(event));
     for(rec of event['Records']) {
-        let parsed = parseInput(rec.kinesis.data);
+        let decoded = decodeInput(rec.kinesis.data);
+        let parsed = JSON.parse(decoded);
         console.log(parsed);
 
         console.log(`get subscribers for ${parsed.eventType}`);
         let subscribers = await getSubscribersForEvent(parsed.eventType);
-        console.log(subscribers);
+        if(subscribers.length == 0) {
+            console.log('no subscribers');
+            break;
+        }
+
+        let zipped = subscribers.map(s => {return {s, decoded}});
+        await Promise.all(zipped.map(tuple => {return write2river(tuple)}));
     }
 };
 
